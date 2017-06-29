81e172d8d0fada9a230c871b0b4fca171d8f8480
fllencina <lencina.fernanda@yahoo.com.ar>
2017-06-28 21:59:07 -0300

    segundo commit
    ejercicios 3 y 4

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,18 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//var dato=0;
+	//dato="octavio";
+	
+	/*dato=document.getElementById('elNombre').value;
+	alert(dato);*/
+
+	document.getElementById('elNombre').value="desconocido";
+	
+}
+
+//usamos promt para ingresar variable  entrada de dato
+//usamos alert para mostrar variable  salida de dato
+
+//document.getElementBy... para que busque datos de otro documento en este caso del html
