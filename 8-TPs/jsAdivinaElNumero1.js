811870a067c40c1b6e407b462b468a577690eaa9
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-06 16:59:32 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -9,18 +9,38 @@ de no ser igual se debe informar si “falta…”  para llegar al número secre
 */
 var numeroSecreto; 
 var contadorIntentos;
-
+var numero;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	numeroSecreto=Math.floor((Math.random() * 100) + 1);
+		alert(numeroSecreto );
+	numero=document.getElementById('numero').value;
 
+	contadorIntentos=0;
+	document.getElementById('intentos').value=contadorIntentos;
 }
 
 function verificar()
 {
+	numero=document.getElementById('numero').value;
+
+	if (numero==numeroSecreto)
+	{
+		alert("Usted es un ganador");
+	}
+	else
+		if (numero>numeroSecreto)
+		{
+			alert("Se pasó...");
+		}
+		else
+		{
+			alert("Falta...");
+		}
 	
-	
+	contadorIntentos=contadorIntentos+1;
+	document.getElementById('intentos').value=contadorIntentos;
+
+
 }
