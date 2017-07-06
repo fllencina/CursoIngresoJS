09f59602f0d6409d2100beb29a39d6df947f26ec
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-06 10:45:46 -0300

    ejercicio10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,29 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota=Math.floor((Math.random() * 10) + 1);
 
+	alert(nota);
+
+
+
+	if(nota==10 || nota==9)
+	{
+		alert("EXCELENTE");
+	}
+
+	else
+	{
+		if(nota>4)
+		{
+			alert("Aprobado");
+
+		}
+		 else
+		 {
+		 	alert("Vamos, la proxima se puede");
+		 }
+	 }
+		 
+	
 }//FIN DE LA FUNCIÓN
