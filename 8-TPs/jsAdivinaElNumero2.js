7698bcaab76e37c16079ac22f7aa3b28c4a7d5b2
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-07 16:18:32 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,72 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+var numero;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
 	
+	numeroSecreto=Math.floor((Math.random() * 100) + 1);
+		alert(numeroSecreto );
+	//numero=document.getElementById('numero').value;
 
+	contadorIntentos=0;
+	document.getElementById('intentos').value=contadorIntentos;
 }
 
 function verificar()
 {
+	contadorIntentos=contadorIntentos+1;
+	document.getElementById('intentos').value=contadorIntentos;
+
+
+	numero=document.getElementById('numero').value;
+
+	if (numero==numerosecreto && contadorIntentos==1)
+	{
+		alert("usted es un Psíquico");
+	}
+	else
+		if (numero==numerosecreto&&contadorIntentos==2)
+		{
+			alert("excelente percepción");
+		}
+		else
+		{
+			if (numero==numerosecreto && contadorIntentos==3)
+			{
+				alert("Esto es suerte");
+
+			}
+			else
+				if(numero==numerosecreto && contadorIntentos==4)
+				{
+					alert("Excelente técnica");
+				}
+				else
+				{
+					if(numero==numerosecreto && contadorIntentos==5)
+					{
+						alert("usted está en la media");
+
+					}
+					else
+						if(numero==numerosecreto && contadorIntentos>5 && contadorIntentos<10)
+						{
+							alert("falta técnica");
+						}
+						else
+						{
+							alert("afortunado en el amor!!");
+						}
+				}
+
+		}
 	
 	
 
+
 }
+
+
