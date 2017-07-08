6f82af8ba443d29582bd389a49b8214c1c8a4215
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-07 22:13:06 -0300

    commt switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,53 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+/*
+		if(mesDelAño=="Enero")
+		{
+			alert("que comiences bien el año!!");
+		}
+		
+		else
+		{
+			if(mesDelAño=="Marzo")
+			{
+				alert("a clases!!!");
+			}
+			else
+			{
+				if(mesDelAño=="Julio")
+				{
+					alert("se vienen las vacaciones!!!");
 
+				}
+				else
+				{
+					if(mesDelAño=="Diciembre")
+					{
+						alert("Felices Fiestas!!!")
+					}
+				}
+			}
+		}
+		*/
 
-
+	switch (mesDelAño) 
+	{
+    case "Enero":
+       	alert("que comiences bien el año!!");
+        break;
+    case "Marzo":
+       	alert("a clases!!!");
+        break;
+    case "Julio":
+       alert("se vienen las vacaciones!!!");
+        break;
+    case "Diciembre":
+        alert("Felices Fiestas!!!")
+        break;
+   
+	}
 
 }//FIN DE LA FUNCIÓN
+
+
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch (mesDelAño) 
+	{
+   
+    case "Febrero": 
+    	alert(" Este mes tiene 28 días.");
+    	break;
+
+	case "Abril": 
+	case "Junio": 
+	case "Septiembre": 
+	case "Nioviembre": 
+	    	alert(" Este mes tiene 30 días.");
+	    	break;
+    default: 
+        alert("Este mes tiene 31 días");
+        
+
+}
 
 
 
