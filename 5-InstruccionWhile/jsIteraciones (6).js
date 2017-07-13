aa2c02b13b3bc2acc5c8fc9ce580c37ee01573ca
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-12 22:08:33 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,38 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	/*while (true)
+	{
+		console.log("Hola"); //se ejecutaria 1 sola vez
+		break;// me saca de la instruccion
+		console.log("chau"); // no se ejecutara nunca porque esta despues de un break
+
+	}
+	*/
+	while (contador<5)
+	{
+		contador=contador+1;
+		numero=prompt("ingrese un numero: ");
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+	}
 
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
+
+/* 
+var salir=no;
+
+while (salir!="si")
+{
+	console.log("hola");
+	salir=si;
+	console.log("chau");
+
+}
+*/
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,22 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+	while (respuesta=="si")
+	{
+		contador=contador+1;
+		numero=prompt("ingrese el " + contador + "ª numero");
+		numero=parseInt(numero);
+
+		acumulador=acumulador+numero;
+	
+
+		respuesta=prompt("ingrese si para continuar");
+	}
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
