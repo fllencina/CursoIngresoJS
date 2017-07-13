b3d55c6f0fbff30134645250713c010d92874c3a
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-13 16:53:52 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -7,7 +7,74 @@ D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
 F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
  */
+
+ var Edad;
+ var Sexo;
+ var EstadoCivil;
+ var Sueldo;
+ var legajo;
+ var Nacionalidad;
+
+Edad=0;
 function ComenzarIngreso () 
 {
+	while(!(Edad>=18 && Edad<= 90 ))
+	{
+		Edad=prompt("por favor ingrese Edad: "  );
+		Edad=parseInt(Edad);
+		
+		console.log("Edad:" + Edad);
+	}
+
+	document.getElementById('Edad').value=Edad + " Años";
+
+	
+
+	while(!(Sexo=='f' || Sexo=='m' ))
+	{
+		Sexo=prompt("por favor ingrese Sexo : \n “M” para masculino y “F” para femenino");
+	
+	}
+
+	document.getElementById('Sexo').value=Sexo;
+
+	 while (!(EstadoCivil>0 &&EstadoCivil<5))
+	{
+		EstadoCivil=prompt("Por favor ingrese Estado civil: \n 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+		EstadoCivil=parseInt(EstadoCivil);
+
+	}
+	document.getElementById('EstadoCivil').value=EstadoCivil;
+	
+
+	while (!(Sueldo>=8000))
+	{
+		Sueldo=prompt("por favor ingrese Sueldo");
+		Sueldo=parseInt(Sueldo);
+	}
+
+	document.getElementById('Sueldo').value=Sueldo;
+
+
+
+	while (!(Legajo>=1000 && Legajo <=9999))
+	{
+		Legajo=prompt("por favor ingrese legajo");
+		Legajo=parseInt(Legajo);
+	}
+
+	document.getElementById('Legajo').value=Legajo;
+
+	while (!(Nacionalidad=='a' || Nacionalidad=='e'|| Nacionalidad=='n'))
+	{
+		Nacionalidad=prompt("por favor ingrese Nacionalidad : \n “A” para argentinos, “E” para extranjeros, “N” para nacionalizados");
+
+	}
+
+	document.getElementById('Nacionalidad').value=Nacionalidad;
+
+
+
+	
  
 }
