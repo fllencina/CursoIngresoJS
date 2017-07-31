ad02ec702cada306e79f75d392609dbccf493045
Fernanda Lencina <lencina.fernanda@yahoo.com.ar>
2017-07-31 09:13:29 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -3,7 +3,14 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
+laHora=parseInt(laHora);
+ 		
+
 //alert (laHora);
+if (laHora<12&&laHora>6)
+{
+	alert("Es de mañana");
+}
 	
 	
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,48 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
+laHora=parseInt(laHora);
+ 		
+
+//alert (laHora);
+	if (laHora>=7&&laHora<=11)
+	{
+		alert("Es de mañana");
+	}
+	else
+	{
+		if(laHora>=12&&laHora<=19)
+		{
+			alert("Es de tarde");
+		}
+		
+		else
+		{
+			switch (true)
+			{
+				case (laHora>=20&&laHora<=24):
+				case (laHora>=0&&laHora<=6):
+				alert("Es de noche");
+				break;
+
+				default:
+				alert("La hora no existe");
+				break;
+
+			}
+			
+			{
+				
+
+
+
+
+			}
+		}
+	
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
